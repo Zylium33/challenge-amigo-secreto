1cd20e2148f825355556b661161ece9cbc4148a5
jorge <ilimitado360@gmail.com>
2025-08-12 17:45:23 -0600

    Muestra los nombres agregados al usuario

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ let amigos = [];
 function agregarAmigo() {
     let amigo = document.getElementById("amigo").value; 
     amigos.push(amigo);
+    
+    let nuevoLi = document.createElement("li");
+    nuevoLi.textContent = amigo;
+    document.getElementById("listaAmigos").appendChild(nuevoLi);
+    
     document.getElementById("amigo").value = "";    
+    document.getElementById("amigo").focus();    
     console.log(amigos);
 }

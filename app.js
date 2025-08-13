6ed7c85bd01d63e98a59d4117d92ba219ab1d984
jorge <ilimitado360@gmail.com>
2025-08-12 20:45:57 -0600

    Genera alertas de uso

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
+//Array vacio para nombres
 let amigos = [];
 
 function agregarAmigo() {
+    //Agrega los nombres a el array 
     let amigo = document.getElementById("amigo").value; 
+    if (amigo === "") {
+        alert("Favor de escribir un nombre");
+        return;
+    }
+    if (tieneNumero(amigo)) {
+        alert("No se pueden ingresar numeros");
+        return;
+    }
     amigos.push(amigo);
     
+    //Crea el elemento visual de la lista 
     let nuevoLi = document.createElement("li");
     nuevoLi.textContent = amigo;
     document.getElementById("listaAmigos").appendChild(nuevoLi);
     
+    //Limpia el input
     document.getElementById("amigo").value = "";    
+    //Mantiene el cursor en el input
     document.getElementById("amigo").focus();    
     console.log(amigos);
+}
+
+function tieneNumero(caca) {
+    return /\d/.test(caca);
 }
